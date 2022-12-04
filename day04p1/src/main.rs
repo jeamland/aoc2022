6ec251a1c0eac1220eda91d372f70269e4755a74
Benno Rice <benno@jeamland.net>
2022-12-04 19:06:44 +1100

    Day 4

---
+++ day04p1/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use itertools::Itertools;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let input = BufReader::new(File::open(&args[1]).unwrap());
+
+    let mut overlaps = 0;
+
+    for line in input.lines() {
+        let line = line.unwrap();
+
+        let (first, second) = line
+            .split(',')
+            .map(|r| {
+                let (start, end) = r
+                    .split('-')
+                    .map(|n| usize::from_str_radix(n, 10).unwrap())
+                    .collect_tuple()
+                    .unwrap();
+                (start..=end).collect::<HashSet<usize>>()
+            })
+            .collect_tuple()
+            .unwrap();
+
+        if first.is_subset(&second) || second.is_subset(&first) {
+            overlaps += 1;
+        }
+    }
+
+    println!("Overlapping assignments: {overlaps}")
+}
